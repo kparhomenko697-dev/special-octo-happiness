c746bef486d862cf0954476238271778275c36ce
kparhomenko697-dev <kparhomenko697@gmail.com>
2026-02-21 17:04:26 +0200

    Lesson2 commit

---
+++ Leson2.py
@@ -0,0 +1,36 @@
+"""
+def назва_функції(аргументи):
+    код функції
+
+    назва_функції(значчення)
+
+from hashlib import new
+
+# однорядковий коментар
+print('''
+однорядковий коментар
+однорядковий коментар
+однорядковий коментар
+''')
+"""
+
+
+def first_function():
+    print('Hello students!')
+
+first_function()
+print(first_function)
+
+def second_function():
+    hello = "Hello students!"
+    name = input("What is your name? ")
+    return hello, name
+
+    return hello
+
+print(second_function)
+print(second_function())
+#print(hello, name) ---> print("Hello student!", "Serchii")
+
+
+
