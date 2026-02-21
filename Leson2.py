6d9741665917e2fd85721c5766c91197f3790061
kparhomenko697-dev <kparhomenko697@gmail.com>
2026-02-21 18:41:05 +0200

    Lesson2.commit2

--- Leson2.py
+++ Leson2.py
@@ -15,6 +15,7 @@ print('''
 """
 
 
+
 """
 def first_function():
     print('Hello students!')
@@ -35,6 +36,7 @@ print(second_function())
 """
 
 
+
 """
 def hello(arg_1, arg_2):
     return arg_1 + arg_2
@@ -49,6 +51,9 @@ y = "Step"
 print(hello(x, y))
 """
 
+
+
+"""
 def _s_triangle(a,h):
     s = .5 * a * h
     return s
@@ -56,3 +61,90 @@ def _s_triangle(a,h):
 print(f"площа трикутинка s = {s_triangle(5, 6)}")
 print(f"площа трикутинка s ="
       f" {s_triangle(int(input('a=')), int(input('h=')))}")
+"""
+
+
+
+"""
+def calc(var_1, var_2, var_3):
+    return var_1 * 3 + var_2 / 4 + var_3 ** 1.5
+
+result_1 = calc(1, 2, 3)
+print(f"result_1 = {calc(1, 2, 3)}")
+
+result_2 = calc(var_1=1,var_2= 2,var_3= 3)
+print(f"result_2 = {calc(var_1=1,var_2= 2, var_3=3)}")
+
+result_3 = calc(1,  2,  3)
+print(f"result_3 = {calc(1, 2, var_3=3)}")
+
+print(f"result_4 = {calc(1, 2, var_3=3)}")
+"""
+
+
+
+"""
+import random
+
+def coin_simulator():
+    coins = random.randint(0, 1)
+    if coins == 0:
+        print("Решка")
+    else:
+        print("Орел")
+
+for i in range(5):
+    coin_simulator()
+ """
+
+
+
+"""
+var_1 = 5
+
+def function_1():
+    global var_2
+    var_1 = 10
+    print(f"function_1 --> var_1 = {var_1}")
+    var_2 = 4
+
+function_1()
+print(f"global var_1 --> var_1 = {var_1}")
+print(f"var_2 = {var_2}")
+  """
+
+"""
+var_1 = 5
+
+def first():
+    var_1 = 10
+    
+    def second():
+        print(f"nonlocal var_1 = {var_1}")
+        
+        second()
+first()
+print(f" global var_1 --> var_1 = {var_1}")
+"""
+
+
+
+var_2 = 15
+def first():
+    var_1 = 10
+
+    def second():
+        nonlocal var_1
+        var_1 = 1
+        global var_2
+        var_2 = 2
+
+    second()
+    print(f"local --> var_1 = {var_1}")
+
+first()
+print(f"global  --> var_2 = {var_2}")
+
+
+
+
